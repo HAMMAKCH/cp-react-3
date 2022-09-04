21fd11084c072bf01eae33499cd3cdf31a8273aa
HAMMAKCH <hamakch12@gmail.com>
2022-09-04 11:54:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import "./App.css"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'block', 
+                  width: 1200, 
+                  padding: 150 ,
+                  fontFamily: 200,
+                  backgroundColor:"gray"}}>
+      <h4>FIRST FORMULAIR WITH REACT</h4>
+      <Form>
+      <Form.Group>
+          <Form.Label>Enter your full name:</Form.Label>
+          <Form.Control type="text" 
+                        placeholder="Enter your full name" width="50%" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Enter your email address:</Form.Label>
+          <Form.Control type="email" 
+                        placeholder="Enter your your email address" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Enter your age:</Form.Label>
+          <Form.Control type="number" placeholder="Enter your age" />
+        </Form.Group>
+        <br></br>
+        <Button  variant="primary" type="submit">
+           Click here to submit form
+        </Button> <br>
+        </br>
+    
+        
+      </Form>
     </div>
   );
 }
+// var y = function (y){
+// addEventListener( "click" , Button )
+//   var y = prompt ('hello world')
 
-export default App;
+//   }
+   
